f01e388230877647911269a4a548820637d43f8c
Jorge Ribeiro <jorge.d.ribeiro@outlook.com>
2026-04-22 09:49:12 +0100

    WIP: OpenRouter tests

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # [Heracles devel](https://github.com/JorgeDFR/heracles_devel)
 
 TBC
+
+## Install
+
+```bash
+mkdir -p ~/devel
+cd ~/devel
+git clone git@github.com:JorgeDFR/heracles_devel.git
+git submodule update --init --recursive
+```
+
+## Run
+
+```bash
+cd ~/devel/heracles_devel
+./run_heracles
+```
--- run_heracles.sh
+++ run_heracles.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-docker compose -f docker/docker-compose.yaml up -d ollama
+# docker compose -f docker/docker-compose.yaml up -d ollama
 
-#docker compose -f docker/docker-compose.yaml up -d neo4j hydra_visualization
+# docker compose -f docker/docker-compose.yaml run --rm cli
+# docker compose -f docker/docker-compose.yaml run --rm heracles_dev
 
-#docker compose -f docker/docker-compose.yaml run --rm chatdsg
-docker compose -f docker/docker-compose.yaml run --rm cli
-docker compose -f docker/docker-compose.yaml run --rm heracles_dev
-
-#docker compose -f docker/docker-compose.yaml down hydra_visualization
+# docker compose -f docker/docker-compose.yaml up -d neo4j hydra_visualization
+docker compose -f docker/docker-compose.yaml run --rm chatdsg
+# docker compose -f docker/docker-compose.yaml down hydra_visualization
