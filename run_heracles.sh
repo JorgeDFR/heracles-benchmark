7c5650fedd83aa3d6c291303731325f150416515
Jorge Ribeiro <jorge.dfr@outlook.com>
2026-07-21 09:32:14 +0100

    WIP: Initial tests hugging face custom local server

--- run_heracles.sh
+++ run_heracles.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# docker compose -f docker/docker-compose.yaml up -d neo4j
 # docker compose -f docker/docker-compose.yaml up -d ollama
+# docker compose -f docker/docker-compose.yaml up -d huggingface
 
-# docker compose -f docker/docker-compose.yaml run --rm cli
-# docker compose -f docker/docker-compose.yaml run --rm heracles_dev
+docker compose -f docker/docker-compose.yaml run --rm cli
+docker compose -f docker/docker-compose.yaml run --rm heracles_dev
+
+# docker compose -f docker/docker-compose.yaml run --rm chatdsg
 
 # docker compose -f docker/docker-compose.yaml up -d neo4j hydra_visualization
-docker compose -f docker/docker-compose.yaml run --rm chatdsg
+# docker compose -f docker/docker-compose.yaml run --rm chatdsg
 # docker compose -f docker/docker-compose.yaml down hydra_visualization
